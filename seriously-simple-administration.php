f4a1734b0e2e486013351436e73c029899aeb10b
Jonathan Bossenger <jonathanbossenger@gmail.com>
2017-09-01 12:20:15 +0200

    Version bump

--- seriously-simple-administration.php
+++ seriously-simple-administration.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Seriously Simple Administration
- * Version: 1.0.2
+ * Version: 1.0.3
  * Plugin URI: http://jonathanbossenger.com/
  * Description: Basic admin for Seriously Simple Podcasting/Hosting
  * Author: Jonathan Bossenger
